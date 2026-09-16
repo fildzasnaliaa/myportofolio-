944c0fe353fc18b85f4322ea4739ccb28cf20007
Fildza Hasnalia Nabila <fhasnalia@gmail.com>
2026-09-16 17:12:20 +0700

    Tugas 2

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import Experience, Project
 
 def show_main(request):
     experiences = Experience.objects.all()
     context = {
-        'npm' : '2506625003',
+        'npm': '2506625003',
         'name': 'Fildza Hasnalia Nabila',
         'class': 'PBP A',
         'experiences': experiences
@@ -18,3 +18,11 @@ def show_experience(request):
         'experiences': experiences
     }
     return render(request, "experience.html", context)
+
+def show_project(request):
+    projects = Project.objects.all()
+    context = {
+        'name': 'Fildza Hasnalia Nabila',
+        'projects': projects
+    }
+    return render(request, "projects.html", context)
